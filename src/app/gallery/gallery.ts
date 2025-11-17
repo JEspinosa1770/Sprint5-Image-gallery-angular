acc8dbd90a32d838bb1500d1992da97eb8eba840
JEspinosa1770 <jespinosa1770@gmail.com>
2025-11-17 20:07:41 +0100

    feat: add data to the array of Image.

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -8,5 +8,78 @@ import { Image } from '../models/image'
   styleUrl: './gallery.css',
 })
 export class Gallery {
-  imagesArray: Image[] = [];
+  imagesArray: Image[] = [
+    {
+      id: 1,
+      source: "https://picsum.photos/id/964/300/200.jpg",
+      alt: "Image 1",
+      title: "",
+      selected: false,
+      deleted: false
+    },
+    {
+      id: 1,
+      source: "https://picsum.photos/id/864/300/200.jpg",
+      alt: "Image 1",
+      title: "",
+      selected: false,
+      deleted: false
+    },
+    {
+      id: 1,
+      source: "https://picsum.photos/id/764/300/200.jpg",
+      alt: "Image 1",
+      title: "",
+      selected: false,
+      deleted: false
+    },
+    {
+      id: 1,
+      source: "https://picsum.photos/id/664/300/200.jpg",
+      alt: "Image 1",
+      title: "",
+      selected: false,
+      deleted: false
+    },
+    {
+      id: 1,
+      source: "https://picsum.photos/id/564/300/200.jpg",
+      alt: "Image 1",
+      title: "",
+      selected: false,
+      deleted: false
+    },
+    {
+      id: 1,
+      source: "https://picsum.photos/id/464/300/200.jpg",
+      alt: "Image 1",
+      title: "",
+      selected: false,
+      deleted: false
+    },
+    {
+      id: 1,
+      source: "https://picsum.photos/id/364/300/200.jpg",
+      alt: "Image 1",
+      title: "",
+      selected: false,
+      deleted: false
+    },
+    {
+      id: 1,
+      source: "https://picsum.photos/id/264/300/200.jpg",
+      alt: "Image 1",
+      title: "",
+      selected: false,
+      deleted: false
+    },
+    {
+      id: 1,
+      source: "https://picsum.photos/id/164/300/200.jpg",
+      alt: "Image 1",
+      title: "",
+      selected: false,
+      deleted: false
+    }
+  ];
 }
