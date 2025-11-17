477accae9d8811d8c65df367c022b46884992a99
JEspinosa1770 <jespinosa1770@gmail.com>
2025-11-17 21:07:49 +0100

    feat: add imports on app.ts to show gallery and image-item messages.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ImageItem } from './image-item/image-item';
+import { Gallery } from "./gallery/gallery";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageItem, Gallery],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
