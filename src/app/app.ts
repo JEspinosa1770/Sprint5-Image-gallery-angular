ed32c739028b82c5adc9ada516361598c0220521
JEspinosa1770 <jespinosa1770@gmail.com>
2025-11-18 10:46:16 +0100

    fix: delete commented lines in app.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ImageItem } from './image-item/image-item';
 import { Gallery } from "./gallery/gallery";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ImageItem, Gallery],
+  imports: [ImageItem, Gallery],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
