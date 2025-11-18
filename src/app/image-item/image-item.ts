8e34f4939d47771b7d06169db90a4ef0ff33eb6d
JEspinosa1770 <jespinosa1770@gmail.com>
2025-11-18 10:42:06 +0100

    feat: change @Input with reactive signals.

--- src/app/image-item/image-item.ts
+++ src/app/image-item/image-item.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
+import { Image } from '../models/image';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
 
 @Component({
   selector: 'app-image-item',
   imports: [],
-  templateUrl: './image-item.html',
+  template: `
+    <div class="gallery-item">
+      <img [src]="image().source" [alt]="image().alt" [title]="image().title">
+      <p>{{ image().title }} - {{isFeatured()}}</p>
+    </div>
+  `,
+  // templateUrl: './image-item.html',
   styleUrl: './image-item.css',
+  // schemas: CUSTOM_ELEMENTS_SCHEMA
 })
 export class ImageItem {
-
+  // @Input({ required: true }) image!: Image;
+  // @Input() isFeatured: boolean = false;
+  image = input.required<Image>();
+  isFeatured = input<Boolean>();
 }
