1e15003e26f87307883b24966c722ce56ab7fdc7
JEspinosa1770 <jespinosa1770@gmail.com>
2025-11-18 10:45:25 +0100

    feat: correcting the data and importing the ImageItem.

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { Image } from '../models/image'
-
+import { ImageItem } from '../image-item/image-item';
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [ImageItem],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css',
 })
@@ -13,71 +13,71 @@ export class Gallery {
       id: 1,
       source: "https://picsum.photos/id/964/300/200.jpg",
       alt: "Image 1",
-      title: "",
+      title: "Image 1",
       selected: false,
       deleted: false
     },
     {
-      id: 1,
+      id: 2,
       source: "https://picsum.photos/id/864/300/200.jpg",
-      alt: "Image 1",
-      title: "",
+      alt: "Image 2",
+      title: "Image 2",
       selected: false,
       deleted: false
     },
     {
-      id: 1,
+      id: 3,
       source: "https://picsum.photos/id/764/300/200.jpg",
-      alt: "Image 1",
-      title: "",
+      alt: "Image 3",
+      title: "Image 3",
       selected: false,
       deleted: false
     },
     {
-      id: 1,
+      id: 4,
       source: "https://picsum.photos/id/664/300/200.jpg",
-      alt: "Image 1",
-      title: "",
+      alt: "Image 4",
+      title: "Image 4",
       selected: false,
       deleted: false
     },
     {
-      id: 1,
+      id: 5,
       source: "https://picsum.photos/id/564/300/200.jpg",
-      alt: "Image 1",
-      title: "",
+      alt: "Image 5",
+      title: "Image 5",
       selected: false,
       deleted: false
     },
     {
-      id: 1,
+      id: 6,
       source: "https://picsum.photos/id/464/300/200.jpg",
-      alt: "Image 1",
-      title: "",
+      alt: "Image 6",
+      title: "Image 6",
       selected: false,
       deleted: false
     },
     {
-      id: 1,
+      id: 7,
       source: "https://picsum.photos/id/364/300/200.jpg",
-      alt: "Image 1",
-      title: "",
+      alt: "Image 7",
+      title: "Image 7",
       selected: false,
       deleted: false
     },
     {
-      id: 1,
+      id: 8,
       source: "https://picsum.photos/id/264/300/200.jpg",
-      alt: "Image 1",
-      title: "",
+      alt: "Image 8",
+      title: "Image 8",
       selected: false,
       deleted: false
     },
     {
-      id: 1,
+      id: 9,
       source: "https://picsum.photos/id/164/300/200.jpg",
-      alt: "Image 1",
-      title: "",
+      alt: "Image 9",
+      title: "Image 9",
       selected: false,
       deleted: false
     }
